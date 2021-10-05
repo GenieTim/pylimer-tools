dacc3cdd593819c15e959b0fd30af0d9d0469624
Tim Bernhard <tim@bernhard-webstudio.ch>
2021-10-05 15:06:13 +0200

    Initial commit containing some (to be refactored) code

---
+++ bin/build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")/.." || exit
+
+# Make sure you have the latest version of PyPA’s build installed:
+# python3 -m pip install --upgrade build
+# python3 -m pip install --upgrade twine
+
+python3 -m build
+
+python3 -m twine upload dist/*
