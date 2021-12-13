eee8d3f3c6b15c5c87be2d3cc4551e8a32813bca
Tim Bernhard <tim@bernhard.dev>
2021-12-13 15:01:06 +0100

    Preliminary implementation of lazy dump file reader

--- src/pylimer_tools_cpp/utils/DumpFileParser.cpp
+++ src/pylimer_tools_cpp/utils/DumpFileParser.cpp
@@ -1,114 +1,144 @@
-#include <string>
-#include <vector>
-#include <algorithm>
-#include "StringUtil.h"
 #include "DumpFileParser.h"
 #include "../entities/Universe.h"
 #include "../entities/UniverseSequence.h"
-#include <map>
-#include <cctype>
+#include "StringUtil.h"
+#include <algorithm>
 #include <any>
+#include <cctype>
 #include <cstring>
 #include <filesystem>
-#include <fstream>      // std::ifstream
+#include <fstream> // std::ifstream
+#include <map>
+#include <string>
+#include <vector>
 
-namespace pylimer_tools
-{
-  namespace utils
-  {
-    // types
-    typedef std::map<std::string, std::vector<pylimer_tools::utils::CsvTokenizer>> data_item_t;
+namespace pylimer_tools {
+namespace utils {
+// types
+typedef std::map<std::string, std::vector<pylimer_tools::utils::CsvTokenizer>>
+    data_item_t;
 
-    void DumpFileParser::read(const std::string filePath)
-    {
-      if (!std::filesystem::exists(filePath))
-      {
-        throw std::invalid_argument("File to read (" + filePath + ") does not exist.");
-      }
+/**
+ * @brief Initialize the parser to read from a certain file path
+ *
+ * @param filePath
+ */
+void DumpFileParser::startReading(const std::string filePath) {
 
-      std::string line;
-      std::ifstream file;
-      file.open(filePath);
+  if (!std::filesystem::exists(filePath)) {
+    throw std::invalid_argument("File to read (" + filePath +
+                                ") does not exist.");
+  }
 
-      if (!file.is_open()) {
-        throw std::invalid_argument("File to read file (" + filePath + "): failed to open.");
-      }
+  std::string line;
+  this->file.open(filePath);
 
-      // read everything until the first key
-      while (getline(file, line))
-      {
-        line = pylimer_tools::utils::trimLineOmitComment(line);
-        // skip empty lines: break when not empty
-        if (!line.empty())
-        {
-          break;
-        }
-      }
+  if (!this->file.is_open()) {
+    throw std::invalid_argument("File to read file (" + filePath +
+                                "): failed to open.");
+  }
 
-      // Assemble CSV data for all keys
-      std::string newGroupKey(line); // new group key: key for a new timestep (group)
-      std::string currentKey = this->cleanHeader(line);
-      data_item_t dataItem;
-
-      while (getline(file, line))
-      {
-        line = pylimer_tools::utils::trimLineOmitComment(line);
-        // skip empty lines
-        if (line.empty())
-        {
-          continue;
-        }
-        // new header
-        if (pylimer_tools::utils::startsWith(line, "ITEM:"))
-        {
-          currentKey = this->cleanHeader(line);
-        }
-        else
-        {
-          dataItem[currentKey].push_back(pylimer_tools::utils::CsvTokenizer(line));
-        }
-
-        if (line == newGroupKey)
-        {
-          // new timestep
-          this->data.push_back(dataItem);
-          dataItem = data_item_t();
-        }
-      }
-      // last timestep
-      this->data.push_back(dataItem);
-
-      file.close();
-    }
-
-    std::string DumpFileParser::cleanHeader(std::string headerToClean)
-    {
-      // "ITEM: ".size() = 6
-      headerToClean.erase(0, 6);
-      pylimer_tools::utils::CsvTokenizer tokenizer(headerToClean);
-
-      std::string newHeader = "";
-      std::vector<std::string> columns;
-      for (size_t i = 0; i < tokenizer.getLength(); ++i)
-      {
-        std::string beg = tokenizer.get<std::string>(i);
-        if (isUpper(beg))
-        {
-          newHeader.append(beg);
-          newHeader.append(" ");
-        }
-        else
-        {
-          columns.push_back(beg);
-        }
-      }
-      newHeader = pylimer_tools::utils::rtrim(newHeader);
-      if (!this->headerColMap.contains(newHeader))
-      {
-        this->headerColMap.insert_or_assign(newHeader, columns);
-      }
-
-      return newHeader;
+  // read everything until the first key
+  while (getline(this->file, line)) {
+    line = pylimer_tools::utils::trimLineOmitComment(line);
+    // skip empty lines: break when not empty
+    if (!line.empty()) {
+      break;
     }
   }
+
+  // Assemble CSV data for all keys
+  this->newGroupKey = line; // new group key: key for a new timestep (group)
+  this->currentLine = line; // current line
+};
+
+/**
+ * @brief Read N timesteps
+ *
+ * @param N the nr of groups to read; a negative value results in all groups
+ * being read.
+ */
+void DumpFileParser::readNGroups(const int N) {
+  if (this->newGroupKey.empty()) {
+    throw std::runtime_error("Starting reading first, specify the file");
+  }
+
+  int groupsRead = 0;
+  std::string currentKey = this->cleanHeader(this->currentLine);
+  data_item_t dataItem;
+  std::string line = this->currentLine;
+
+  while (getline(this->file, line) && (N < 0 || groupsRead < N)) {
+    line = pylimer_tools::utils::trimLineOmitComment(line);
+    // skip empty lines
+    if (line.empty()) {
+      continue;
+    }
+    // new header
+    if (pylimer_tools::utils::startsWith(line, "ITEM:")) {
+      currentKey = this->cleanHeader(line);
+    } else {
+      dataItem[currentKey].push_back(pylimer_tools::utils::CsvTokenizer(line));
+    }
+
+    if (line == newGroupKey) {
+      // new timestep
+      this->data.push_back(dataItem);
+      dataItem = data_item_t();
+      groupsRead += 1;
+    }
+  }
+
+  this->currentLine = line;
+  // last timestep
+  this->data.push_back(dataItem);
+};
+
+/**
+ * @brief Forget the data at a certain index
+ *
+ * @param index
+ */
+void DumpFileParser::forgetAt(const int index) {
+  this->data.erase(this->data.begin() + index);
+};
+
+/**
+ * @brief Read a whole file
+ *
+ * @param filePath
+ */
+void DumpFileParser::read(const std::string filePath) {
+  std::string line;
+  this->startReading(filePath);
+  this->readNGroups(-1);
+  this->finish();
 }
+
+void DumpFileParser::finish() { this->file.close(); }
+
+std::string DumpFileParser::cleanHeader(std::string headerToClean) {
+  // "ITEM: ".size() = 6
+  headerToClean.erase(0, 6);
+  pylimer_tools::utils::CsvTokenizer tokenizer(headerToClean);
+
+  std::string newHeader = "";
+  std::vector<std::string> columns;
+  for (size_t i = 0; i < tokenizer.getLength(); ++i) {
+    std::string beg = tokenizer.get<std::string>(i);
+    if (isUpper(beg)) {
+      newHeader.append(beg);
+      newHeader.append(" ");
+    } else {
+      columns.push_back(beg);
+    }
+  }
+  newHeader = pylimer_tools::utils::rtrim(newHeader);
+  if (!this->headerColMap.contains(newHeader)) {
+    this->headerColMap.insert_or_assign(newHeader, columns);
+  }
+
+  return newHeader;
+}
+} // namespace utils
+} // namespace pylimer_tools
