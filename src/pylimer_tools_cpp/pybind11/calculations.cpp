57a6903cdce9e0362767bc81ef9d221228c89d7e
Tim Bernhard <tim@bernhard-webstudio.ch>
2022-03-04 09:19:06 +0100

    Further progress of documentation improvements

--- src/pylimer_tools_cpp/pybind11/calculations.cpp
+++ src/pylimer_tools_cpp/pybind11/calculations.cpp
@@ -26,7 +26,13 @@ void init_pylimer_bound_calc(py::module_ &m) {
   )pbdoc")
       .def(py::init<pe::Universe, int>())
       .def("configDoOutputSteps",
-           &mehp::MEHPForceRelaxation::configDoOutputSteps)
+           &mehp::MEHPForceRelaxation::configDoOutputSteps, R"pbdoc(
+          Whether to output progress, where, and how often.
+
+          :param outputFile: the file to write the time-steps to.
+          :param outputFrequency: how often to output the time-step. Set to 0 (default) to disable progress output.
+          )pbdoc",
+           py::arg("outputFile"), py::arg("outputFrequency") = 0)
       .def("configDoOutputFinalCoordinates",
            &mehp::MEHPForceRelaxation::configDoOutputFinalCoordinates)
       .def("runForceRelaxation", &mehp::MEHPForceRelaxation::runForceRelaxation,
@@ -37,31 +43,54 @@ void init_pylimer_bound_calc(py::module_ &m) {
           :param maxNrOfSteps: The maximum number of steps to do during the simulation.
           :param tolerance: The tolerance of the force as an exit condition.
           :param Nb2: The denominator in the equation of :math:`\Gamma` (see: :func:`~pylimer_tools_cpp.pylimer_tools_cpp.MEHPForceRelaxation.getGammaEq()`). If -1.0 (default), the network is used for determination.
-          :param 2d: Specify true if you want to evaluate the force relation only in x and y direction.
+          :param is2d: Specify true if you want to evaluate the force relation only in x and y direction.
           )pbdoc",
            py::arg("crosslinkerType") = 2, py::arg("maxNrOfSteps") = 250000,
            py::arg("tolerance") = 1e-8, py::arg("Nb2") = -1.0,
-           py::arg("is2d") = false, py::arg("dt") = 0.05, py::arg("kappa") = 1.0)
-      .def("getVolume", &mehp::MEHPForceRelaxation::getVolume)
-      .def("getNrOfNodes", &mehp::MEHPForceRelaxation::getNrOfNodes)
-      .def("getNrOfSprings", &mehp::MEHPForceRelaxation::getNrOfSprings)
-      .def("getNrOfActiveNodes", &mehp::MEHPForceRelaxation::getNrOfActiveNodes)
+           py::arg("is2d") = false, py::arg("dt") = 0.05,
+           py::arg("kappa") = 1.0)
+      //  .def("getVolume", &mehp::MEHPForceRelaxation::getVolume)
+      .def("getNrOfNodes", &mehp::MEHPForceRelaxation::getNrOfNodes, R"pbdoc(
+           Get the number of nodes considered in this simulation.
+      )pbdoc")
+      .def("getNrOfSprings", &mehp::MEHPForceRelaxation::getNrOfSprings,
+           R"pbdoc(
+           Get the number of springs considered in this simulation.
+      )pbdoc")
+      .def("getNrOfActiveNodes", &mehp::MEHPForceRelaxation::getNrOfActiveNodes,
+           R"pbdoc(
+           Get the number of active nodes remaining after running the simulation.
+      )pbdoc")
       .def("getNrOfActiveSprings",
-           &mehp::MEHPForceRelaxation::getNrOfActiveSprings)
+           &mehp::MEHPForceRelaxation::getNrOfActiveSprings, R"pbdoc(
+           Get the number of active springs remaining after running the simulation.
+      )pbdoc")
       .def("getAverageSpringLength",
-           &mehp::MEHPForceRelaxation::getAverageSpringLength)
+           &mehp::MEHPForceRelaxation::getAverageSpringLength, R"pbdoc(
+           Get the average length of the springs.
+      )pbdoc")
       .def("getGammaEq", &mehp::MEHPForceRelaxation::getGammaEq, R"pbdoc(
           Computes the gamma factor as part of the ANT/MEHP formulism, i.e.:
 
           :math:`\Gamma = \langle\gamma_{\eta}\rangle`, with :math:`\gamma_{\eta} = \frac{\bar{r_{\eta}}^2}{N_{\eta} b^2}`,
           which you can use as :math:`G_{\mathrm{ANT}} = \Gamma \nu k_B T`,
-          where :math:`\eta` is the index of a particular strand, :math:`N_{\eta}` its number of atoms, :math:`b` its mean square bond length,
+          where :math:`\eta` is the index of a particular strand, 
+          :math:`N_{\eta}` is the number of atoms in this strand :math:`\eta`, 
+          :math:`b` its mean square bond length,
           :math:`T` the temperature and :math:`k_B` Boltzmann's constant.
       )pbdoc")
-      .def("getGammaX", &mehp::MEHPForceRelaxation::getGammaX)
-      .def("getGammaY", &mehp::MEHPForceRelaxation::getGammaY)
-      .def("getGammaZ", &mehp::MEHPForceRelaxation::getGammaZ)
-      .def("getNb2", &mehp::MEHPForceRelaxation::getNb2);
+      .def("getGammaX", &mehp::MEHPForceRelaxation::getGammaX, R"pbdoc(
+           Get three times the contribution of the x-direction to :func:`~pylimer_tools_cpp.pylimer_tools_cpp.MEHPForceRelaxation.getGammaEq()`.
+      )pbdoc")
+      .def("getGammaY", &mehp::MEHPForceRelaxation::getGammaY, R"pbdoc(
+           Get three times the contribution of the y-direction to :func:`~pylimer_tools_cpp.pylimer_tools_cpp.MEHPForceRelaxation.getGammaEq()`.
+      )pbdoc")
+      .def("getGammaZ", &mehp::MEHPForceRelaxation::getGammaZ, R"pbdoc(
+           Get three times the contribution of the z-direction to :func:`~pylimer_tools_cpp.pylimer_tools_cpp.MEHPForceRelaxation.getGammaEq()`.
+      )pbdoc")
+      .def("getNb2", &mehp::MEHPForceRelaxation::getNb2, R"pbdoc(
+           Returns the value effectively used in :func:`~pylimer_tools_cpp.pylimer_tools_cpp.MEHPForceRelaxation.getGammaEq()` for :math:`N_{\eta} b^2`.
+      )pbdoc");
 }
 
 #endif /* PYBIND_CALC_H */
--- src/pylimer_tools_cpp/pybind11/writers.cpp
+++ src/pylimer_tools_cpp/pybind11/writers.cpp
@@ -13,18 +13,32 @@ using namespace pylimer_tools::utils;
 
 void init_pylimer_bound_writers(py::module_ &m) {
   py::class_<DataFileWriter>(m, "DataFileWriter")
-      .def(py::init<pe::Universe>(), py::arg("universe"))
-      .def("setUniverseToWrite", &DataFileWriter::setUniverseToWrite,
+      .def(py::init<pe::Universe>(), py::arg("universe"), R"pbdoc(
+           Initialize the writer with the universe to write.
+      )pbdoc")
+      .def("setUniverseToWrite", &DataFileWriter::setUniverseToWrite, R"pbdoc(
+           Re-set the universe to write.
+      )pbdoc",
            py::arg("universe"))
-      .def("configIncludeAngles", &DataFileWriter::configIncludeAngles,
-           py::arg("includeAngles"))
-      .def("configReindexAtoms", &DataFileWriter::configReindexAtoms,
-           py::arg("reindexAtoms"))
+      .def("configIncludeAngles", &DataFileWriter::configIncludeAngles, R"pbdoc(
+           Set whether to include the angles from the universe in the file or not.
+      )pbdoc",
+           py::arg("includeAngles") = true)
+      .def("configReindexAtoms", &DataFileWriter::configReindexAtoms, R"pbdoc(
+           Set whether to reindex the atoms or not. 
+           Re-indexing leads to atom ids being in the range of 1 to the number of atoms.
+      )pbdoc",
+           py::arg("reindexAtoms") = true)
       .def("configCrosslinkerType", &DataFileWriter::configCrosslinkerType,
-           py::arg("crosslinkerType"))
+           R"pbdoc(
+           Set which atom type represents cross-linkers. 
+           Needed in case the moleculeIdx in the output file should have any meaning.
+           (e.g. with :func:`~pylimer_tools_cpp.pylimer_tools_cpp.DataFileWriter.configMoleculeIdxForSwap`).
+      )pbdoc",
+           py::arg("crosslinkerType") = 2)
       .def("configMoleculeIdxForSwap",
            &DataFileWriter::configMoleculeIdxForSwap,
-           py::arg("enableSwappability"), R"pbdoc(
+           py::arg("enableSwappability") = false, R"pbdoc(
                 Swappable chains implies that their `moleculeIdx` in the LAMMPS data file is not 
                 identical per chain, but identical per position in the chain.
                 That's how you can have bond swapping with constant chain length distribution.
