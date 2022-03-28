faabccaab6ee126040e6468c04924a4c182a8bbd
Tim Bernhard <tim@bernhard-webstudio.ch>
2022-03-28 17:53:54 +0200

    Fix issue with wrong path to lcov report

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -16,10 +16,10 @@ cd "$ROOT_DIR/tests" || exit 2
 mkdir -p build
 cd build || exit 5
 # force use of g++ if available for coverage
-CXXCOMPILER=$(which g++ || which clang)
-CCOMPILER=$(which gcc || which clang)
-cmake .. -D CODE_COVERAGE=ON -D LEAK_ANALYSIS=ON -D CMAKE_C_COMPILER="$CCOMPILER" -D CMAKE_CXX_COMPILER="$CXXCOMPILER" || exit 1
-# cmake .. -D CODE_COVERAGE=ON -D LEAK_ANALYSIS=ON || exit 1
+# CXXCOMPILER=$(which g++ || which clang)
+# CCOMPILER=$(which gcc || which clang)
+# cmake .. -D CODE_COVERAGE=ON -D LEAK_ANALYSIS=ON -D CMAKE_C_COMPILER="$CCOMPILER" -D CMAKE_CXX_COMPILER="$CXXCOMPILER" || exit 1
+cmake .. -D CODE_COVERAGE=ON -D LEAK_ANALYSIS=ON || exit 1
 cmake --build . || exit 9
 ASAN_OPTIONS=detect_leaks=1 ./pylimer_tests || exit 6 # -s --durations yes
 make pylimer_tools-gcov
@@ -29,12 +29,14 @@ make pylimer_tools-geninfo
 
 cd "$ROOT_DIR" || exit 8
 
-if command -v npx
+cp tests/build/lcov/data/capture/pylimer_tools.info pylimer_tools_lcoverage.info
+
+if command -v npx;
 then
-  npx -y lcov-badge2 -l "C++ Code Coverage" -o "$ROOT_DIR/.github/cpp-coverage.svg" tests/build/lcov/data/capture/pylimer_tools.info
+  npx -y lcov-badge2 -l "C++ Code Coverage" -o ".github/cpp-coverage.svg" pylimer_tools_lcoverage.info
 fi
 
-exit
+# exit
 # then, build/install project for Python
 python -m pip install --verbose --use-feature=in-tree-build . || exit 3
 
