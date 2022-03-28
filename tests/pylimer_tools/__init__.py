0cf4f97b6885648032ea752a64d68c101b8d9866
Tim Bernhard <tim@bernhard-webstudio.ch>
2022-03-28 17:41:56 +0200

    Add codecov.io as a coverage provider

--- tests/pylimer_tools/__init__.py
+++ tests/pylimer_tools/__init__.py
@@ -1,6 +1,15 @@
 import os
 import sys
 
-sys.path.append(os.path.dirname(__file__))
-# sys.path.append(os.path.dirname(__file__ ) + "/../../src/pylimer_tools/")
-# sys.path.append(os.path.dirname(__file__ ) + "/../../src/pylimer_tools/calc")
+# make sure we use the "live" pylimer tools, not the installed one
+# but only the Python one, for the cpp one, we need the copiled/installed
+import pylimer_tools_cpp
+pylimer_tools_cpp_path = pylimer_tools_cpp.__file__
+sys.path.insert(0, os.path.dirname(__file__) + "/../../src")
+sys.path.insert(0, os.path.dirname(pylimer_tools_cpp_path))
+# sys.path.insert(0, os.path.dirname(__file__) + "/../../src/pylimer_tools/calc")
+# sys.path.insert(0, os.path.dirname(__file__) + "/../../src/pylimer_tools/io")
+# sys.path.insert(0, os.path.dirname(__file__) +
+#                 "/../../src/pylimer_tools/utils")
+
+sys.path.insert(0, os.path.dirname(__file__))
