2d7e0ecf54a472868452dfc46c453c6f6da6c792
Tim Bernhard <tim@bernhard-webstudio.ch>
2022-04-07 19:04:24 +0200

    Adjust generator to use ninja also for cov generation

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -3,8 +3,7 @@
 cd "$(dirname "$0")/.." || exit 10
 ROOT_DIR=$(pwd)
 
-
-# pip/skbuild uses ninja as a generator, 
+# pip/skbuild uses ninja as a generator,
 # however, it uses a bundled one in a virtual env
 # therefore, we need to delete vendor caches
 if [ -d "_skbuild" ]; then
@@ -18,6 +17,7 @@ cd "$ROOT_DIR/tests" || exit 2
 # rm -rf build
 mkdir -p build
 cd build || exit 5
+GENERATOR_BIN="make"
 # force use of g++ if available for coverage
 CXXCOMPILER=$(which g++ || which clang)
 CCOMPILER=$(which gcc || which clang)
@@ -31,15 +31,16 @@ else
 fi
 if command -v ninja; then
   ADDITIONALFLAGS=("${ADDITIONALFLAGS[@]}" "-GNinja")
+  GENERATOR_BIN="ninja"
 fi
 cmake .. -D CODE_COVERAGE=ON -D LEAK_ANALYSIS=OFF "${ADDITIONALFLAGS[@]}" || exit 1
 cmake --build . || exit 9
 echo "======== Starting tests ========"
 ASAN_OPTIONS=detect_leaks=1 ./pylimer_tests || exit 6 # -s --durations yes
-make pylimer_tests-gcov
-make test_sources-gcov
-make pylimer_tools-gcov
-make header_tests-gcov
+"$GENERATOR_BIN" pylimer_tests-gcov
+"$GENERATOR_BIN" test_sources-gcov
+"$GENERATOR_BIN" pylimer_tools-gcov
+"$GENERATOR_BIN" header_tests-gcov
 
 # TODO: the following is
 # some sort of fix for header file coverage being assembled incorrectly
@@ -49,13 +50,13 @@ rm pylimer_tools.out/*"#entities#Atom.h.gcov"
 rm ./*"#pylimer_tools_cpp#entities#Atom.h.gcov"
 # ENDTODO
 
-make pylimer_tests-geninfo
-make pylimer_tools-geninfo
-make header_tests-geninfo
-make pylimer_tools-genhtml
+"$GENERATOR_BIN" pylimer_tests-geninfo
+"$GENERATOR_BIN" pylimer_tools-geninfo
+"$GENERATOR_BIN" header_tests-geninfo
+"$GENERATOR_BIN" pylimer_tools-genhtml
 
 cd "$ROOT_DIR" || exit 8
-
+exit
 # copy outside such that pip installation does not remove it
 # cp tests/build/lcov/data/capture/pylimer_tools.info pylimer_tools_lcoverage.info
 
