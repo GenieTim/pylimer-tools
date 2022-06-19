7dd8ac0cbb84882d2a68ef853e4dab88f6e513c3
Tim Bernhard <tim@bernhard-webstudio.ch>
2022-06-19 13:06:10 +0200

    Finally get all tests working again, incl. Python compilation useage

--- vendor/nlopt.CMakeLists.cmake
+++ vendor/nlopt.CMakeLists.cmake
@@ -9,9 +9,6 @@ if (NOT DEFINED nlopt_LOADED)
 		if(WIN32)
 			set(LIBRARY_PREFIX "")
 			set(LIBRARY_SUFFIX ".lib")
-		elseif(APPLE)
-			set(LIBRARY_PREFIX "lib")
-			set(LIBRARY_SUFFIX ".dylib")
 		else()
 			set(LIBRARY_PREFIX "lib")
 			set(LIBRARY_SUFFIX ".a")
@@ -23,19 +20,17 @@ if (NOT DEFINED nlopt_LOADED)
 				GIT_TAG 09b3c2a6da71cabcb98d2c8facc6b83d2321ed71 # 2.7.1
 				PREFIX ${CMAKE_CURRENT_LIST_DIR}/nlopt
 				INSTALL_DIR ${CMAKE_CURRENT_LIST_DIR}/nlopt/nloptLib-install
-				CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_LIST_DIR}/nlopt/nloptLib-install -DCMAKE_INSTALL_LIBDIR=${CMAKE_CURRENT_LIST_DIR}/nlopt/nloptLib-install/lib -DNLOPT_GUILE=OFF -DNLOPT_OCTAVE=OFF -DNLOPT_MATLAB=OFF -DNLOPT_SWIG=OFF
+				CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_LIST_DIR}/nlopt/nloptLib-install -DINSTALL_LIBDIR=${CMAKE_CURRENT_LIST_DIR}/nlopt/nloptLib-install/lib -DCMAKE_INSTALL_LIBDIR=${CMAKE_CURRENT_LIST_DIR}/nlopt/nloptLib-install/lib -DNLOPT_GUILE=OFF -DNLOPT_OCTAVE=OFF -DNLOPT_MATLAB=OFF -DNLOPT_SWIG=OFF -DNLOPT_PYTHON=OFF -DBUILD_SHARED_LIBS=OFF
 				BUILD_COMMAND ${CMAKE_COMMAND} --build ${CMAKE_CURRENT_LIST_DIR}/nlopt/src/nloptLib-build --config Release
 				BUILD_BYPRODUCTS ${CMAKE_CURRENT_LIST_DIR}/nlopt/nloptLib-install/lib/${LIBRARY_PREFIX}nlopt${LIBRARY_SUFFIX}
-				BUILD_BYPRODUCTS ${CMAKE_CURRENT_LIST_DIR}/nlopt/nloptLib-install/lib/${LIBRARY_PREFIX}nlopt.0${LIBRARY_SUFFIX}
-				BUILD_BYPRODUCTS ${CMAKE_CURRENT_LIST_DIR}/nlopt/nloptLib-install/lib/${LIBRARY_PREFIX}nlopt.0.11.1${LIBRARY_SUFFIX}
 				UPDATE_DISCONNECTED ON
 		)
 		# FetchContent_MakeAvailable(nloptLib)
-		add_library(nlopt SHARED IMPORTED)
+		add_library(nlopt STATIC IMPORTED)
 		add_dependencies(nlopt nloptLib)
 		set(nlopt_PREFIX_PATH "${CMAKE_CURRENT_LIST_DIR}/nlopt")
 		if (MSVC)
-			set(nlopt_INCLUDE_DIRS "${nlopt_PREFIX_PATH}/nloptLib-install/include;${nlopt_PREFIX_PATH}/src/nloptLib/msvc/include")
+			set(nlopt_INCLUDE_DIRS "${nlopt_PREFIX_PATH}/nloptLib-install/include" "${nlopt_PREFIX_PATH}/src/nloptLib/msvc/include")
 		else()
 			set(nlopt_INCLUDE_DIRS "${nlopt_PREFIX_PATH}/nloptLib-install/include")
 		endif()
@@ -43,7 +38,7 @@ if (NOT DEFINED nlopt_LOADED)
 		if (NOT nlopt_LIBRARIES)
 			# message("WARNING: nlopt_LIBRARIES empty")
 			# TODO: this is somewhat unreliable
-			set(nlopt_LIBRARIES "${nlopt_PREFIX_PATH}/nloptLib-install/lib/${LIBRARY_PREFIX}nlopt${LIBRARY_SUFFIX}" "${nlopt_PREFIX_PATH}/nloptLib-install/lib/${LIBRARY_PREFIX}nlopt.0${LIBRARY_SUFFIX}" "${nlopt_PREFIX_PATH}/nloptLib-install/lib/${LIBRARY_PREFIX}nlopt.0.11.1${LIBRARY_SUFFIX}")
+			set(nlopt_LIBRARIES "${nlopt_PREFIX_PATH}/nloptLib-install/lib/${LIBRARY_PREFIX}nlopt${LIBRARY_SUFFIX}")
 			# file(GLOB_RECURSE nlopt_LIBRARIES "${nlopt_PREFIX_PATH}/*.a")
 		endif()
 		message("Hoping nlopt_LIBRARIES will be compiled to: ${nlopt_LIBRARIES}")
