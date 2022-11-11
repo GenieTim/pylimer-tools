e9434a36efdaf1f1e9f911f1cc1d8c0cfdf5ee63
Tim Bernhard <tim@bernhard.dev>
2022-11-11 12:32:22 +0100

    Hide one more strange pybinNext step trying tod return inference inability

--- src/pylimer_tools_cpp/pybind11/calculations.cpp
+++ src/pylimer_tools_cpp/pybind11/calculations.cpp
@@ -830,13 +830,13 @@ init_pylimer_bound_calc(py::module_& m)
           Returns the pressure at the current state of the simulation.
      )pbdoc")
     .def("addSlipLinks",
-         py::overload_cast<const std::vector<size_t>,
-                           const std::vector<size_t>,
-                           const std::vector<double>,
-                           const std::vector<double>,
-                           const std::vector<double>,
-                           const std::vector<double>,
-                           const std::vector<double>,
+         py::overload_cast<const std::vector<size_t>&,
+                           const std::vector<size_t>&,
+                           const std::vector<double>&,
+                           const std::vector<double>&,
+                           const std::vector<double>&,
+                           const std::vector<double>&,
+                           const std::vector<double>&,
                            const bool>(&mehp::MEHPForceBalance2::addSlipLinks),
          R"pbdoc(
           Add the slip-links
