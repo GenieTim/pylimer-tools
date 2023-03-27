e91e3887b887b8f1157d601d49ec5bcac1d6e0dc
Tim Bernhard <tim@bernhard-webstudio.ch>
2023-03-27 10:37:45 +0200

    Add/fix some of the tests for the new file readers

--- src/pylimer_tools/io/readLammpsOutputFile.py
+++ src/pylimer_tools/io/readLammpsOutputFile.py
@@ -11,12 +11,12 @@ def readLogFile(filepath) -> pd.DataFrame:
     return extractThermoParams(filepath, header=None, textsToRead=1e5)
 
 
-def readDumpFile(filepath, structureFile) -> UniverseSequence:
+def readDumpFile(dataFile, dumpFile) -> UniverseSequence:
     """
     Read a file with LAMMPS' dump of snapshots of structures into a Universe.
     """
     uS = UniverseSequence()
-    uS.initializeFromDataSequence(structureFile, filepath)
+    uS.initializeFromDumpFile(dataFile, dumpFile)
     return uS
 
 
@@ -53,7 +53,7 @@ def readAveragesFile(filepath) -> pd.DataFrame:
             return readSectionedAveragesFile(filepath)
 
         header_line = line1
-    header_line = header_line.removesuffix("#").strip()
+    header_line = header_line.removeprefix("#").strip()
 
     data = pd.read_csv(filepath, comment="#",
                        names=header_line.split(), sep=" ")
@@ -80,13 +80,13 @@ def readSectionedAveragesFile(filepath) -> pd.DataFrame:
                 "This file was not detected to be a proper sectioned averages file.")
             # return readSectionedAveragesFile(filepath)
 
-        header_line1 = line1.removesuffix("#").strip()
-        header_line2 = line2.removesuffix("#").strip()
+        header_line1 = line1.removeprefix("#").strip()
+        header_line2 = line2.removeprefix("#").strip()
 
         header_line1_split = header_line1.split()
         header_line2_split = header_line2.split()
 
-        if (len(header_line1_split) == 1 and len(header_line2_split)):
+        if (len(header_line1_split) == len(header_line2_split)):
             raise ValueError(
                 "Cannot read this file, as we cannot distinguish between section header and main data")
 
@@ -112,9 +112,9 @@ def readSectionedAveragesFile(filepath) -> pd.DataFrame:
     for key in data.keys():
         split_key = key.split()
         local_dataframe = pd.DataFrame(data[key], columns=header_line2_split)
-        for i, col in enumerate(header_line2_split):
+        for i, col in enumerate(header_line1_split):
             local_dataframe[col] = split_key[i]
-        df = pd.concat([df, local_dataframe], as_index=False)
+        df = pd.concat([df, local_dataframe], ignore_index=True)
 
     # convert all columns of DataFrame
     df = df.apply(pd.to_numeric, errors='ignore')
@@ -187,7 +187,7 @@ def readCorrelationFile(filepath, group_key="Timestep") -> pd.DataFrame:
             row.append(timestep)
             correlatedDataAssembled.append(row)
 
-    cols = header_line.split()
+    cols = header_line.removeprefix("#").strip().split()
     cols.append(group_key)
     correlatedData = pd.DataFrame(
         correlatedDataAssembled, columns=cols)
