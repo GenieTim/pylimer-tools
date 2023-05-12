cd632b9f1b7f33c8c02e84daa17066214725f109
Tim Bernhard <tim@bernhard.dev>
2023-05-12 13:00:05 +0200

    Fix issue of default argument missing

--- tests/pylimer_tools/test_eigen_neighbourlist_benchmark.cpp
+++ tests/pylimer_tools/test_eigen_neighbourlist_benchmark.cpp
@@ -1,6 +1,4 @@
 #include "../../src/pylimer_tools_cpp/entities/EigenNeighbourList.h"
-#include "../../src/pylimer_tools_cpp/entities/Universe.h"
-#include "../../src/pylimer_tools_cpp/entities/UniverseSequence.h"
 #include <catch2/benchmark/catch_benchmark_all.hpp>
 #include <catch2/catch_approx.hpp>
 #include <catch2/catch_test_macros.hpp>
