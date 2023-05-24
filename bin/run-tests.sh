f254f5549197f60e8588d5a9afff4508d3945fe1
Tim Bernhard <tim@bernhard-webstudio.ch>
2023-05-24 09:38:41 +0200

    Separately run [EigenNeighbourList] tests from the others as next attempt to fix stuff

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -48,9 +48,10 @@ cmake .. "${ADDITIONALFLAGS[@]}" || exit 1
 cmake --build . || exit 9
 echo "======== Starting tests ========"
 # MallocNanoZone=0 ASAN_OPTIONS=detect_leaks=1:detect_container_overflow=0:strict_string_checks=1:detect_stack_use_after_return=1:check_initialization_order=1:strict_init_order=1 LSAN_OPTIONS=suppressions=$ROOT_DIR/tests/lsan.supp ./pylimer_tests "[MEHPForceBalance]" || exit 5 # -s --durations yes
-MallocNanoZone=0 ASAN_OPTIONS=detect_leaks=1:detect_container_overflow=0:strict_string_checks=1:detect_stack_use_after_return=1:check_initialization_order=1:strict_init_order=1 LSAN_OPTIONS=suppressions=$ROOT_DIR/tests/lsan.supp ./pylimer_tests --skip-benchmarks --benchmark-samples 10 --durations yes || exit 6 #  "~[long]" -s --durations yes
+MallocNanoZone=0 ASAN_OPTIONS=detect_leaks=1:detect_container_overflow=0:strict_string_checks=1:detect_stack_use_after_return=1:check_initialization_order=1:strict_init_order=1 LSAN_OPTIONS=suppressions=$ROOT_DIR/tests/lsan.supp ./pylimer_tests "[EigenNeighbourList]" --skip-benchmarks --benchmark-samples 10 --durations yes || exit 6 #  -s --durations yes
+MallocNanoZone=0 ASAN_OPTIONS=detect_leaks=1:detect_container_overflow=0:strict_string_checks=1:detect_stack_use_after_return=1:check_initialization_order=1:strict_init_order=1 LSAN_OPTIONS=suppressions=$ROOT_DIR/tests/lsan.supp ./pylimer_tests "~[EigenNeighbourList]" --skip-benchmarks --benchmark-samples 10 --durations yes || exit 7 #  "~[long]" -s --durations yes
 # exit
-MallocNanoZone=0 ASAN_OPTIONS=detect_leaks=1:detect_container_overflow=0:strict_string_checks=1:detect_stack_use_after_return=1:check_initialization_order=1:strict_init_order=1 LSAN_OPTIONS=suppressions=$ROOT_DIR/tests/lsan.supp ./header_tests --skip-benchmarks --benchmark-samples 10 --durations yes || exit 7
+MallocNanoZone=0 ASAN_OPTIONS=detect_leaks=1:detect_container_overflow=0:strict_string_checks=1:detect_stack_use_after_return=1:check_initialization_order=1:strict_init_order=1 LSAN_OPTIONS=suppressions=$ROOT_DIR/tests/lsan.supp ./header_tests --skip-benchmarks --benchmark-samples 10 --durations yes || exit 8
 
 "$GENERATOR_BIN" pylimer_tools-gcov
 find . -name "*Universe.cpp.gcov" -exec cat {} \;
