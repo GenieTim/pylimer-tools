54b444afe5393253361fb0dbda9f76cc94d71f49
Tim Bernhard <tim@bernhard-webstudio.ch>
2023-11-02 13:10:28 +0100

    Add support for reading data files with atom style full

--- src/pylimer_tools_cpp/io/DataFileParser.cpp
+++ src/pylimer_tools_cpp/io/DataFileParser.cpp
@@ -112,6 +112,8 @@ namespace utils {
         case AtomStyle::CHARGE:
           this->readAtomCharge(line);
           break;
+        case AtomStyle::FULL:
+          this->readAtomFull(line);
         default:
           throw std::invalid_argument("This atom style is not supported yet.");
           break;
@@ -257,6 +259,40 @@ namespace utils {
     key = tokenizer.get<int>(0);
     // for now, we just override duplicate keys
     this->masses[key] = tokenizer.get<double>(1);
+  }
+
+  void DataFileParser::readAtomFull(std::string line)
+  {
+    size_t atomId, nx, ny, nz;
+    int atomType, moleculeId;
+    double charge;
+    double x, y, z;
+    int resFound = sscanf(line.c_str(),
+                          "%zd %d %d %le %le %le %le %zd %zd %zd",
+                          &atomId,
+                          &moleculeId,
+                          &atomType,
+                          &charge,
+                          &x,
+                          &y,
+                          &z,
+                          &nx,
+                          &ny,
+                          &nz);
+
+    this->atomIds.push_back(atomId);
+    this->moleculeIds.push_back(moleculeId);
+    this->atomTypes.push_back(atomType);
+    this->atomX.push_back(x);
+    this->atomY.push_back(y);
+    this->atomZ.push_back(z);
+    this->additionalAtomData["charge"].push_back(charge);
+
+    if (resFound > 6) {
+      this->atomNx.push_back(nx);
+      this->atomNy.push_back(ny);
+      this->atomNz.push_back(nz);
+    }
   }
 
   void DataFileParser::readAtomCharge(std::string line)
