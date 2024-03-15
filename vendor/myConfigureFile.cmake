e561941244242c1e7d0557ddac17b8e6e861f57e
Tim Bernhard <tim@bernhard.dev>
2024-03-15 12:51:50 +0100

    Fix issue where versionInfo would not return git hash

---
+++ vendor/myConfigureFile.cmake
@@ -0,0 +1,20 @@
+# Get and compile a config file for this project
+
+# Get the latest abbreviated commit hash of the working branch
+execute_process(
+	COMMAND git log -1 --format=%h
+	WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+	OUTPUT_VARIABLE GIT_COMMIT_HASH
+	OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
+# Get the current working branch
+execute_process(
+  COMMAND git rev-parse --abbrev-ref HEAD
+  WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
+  OUTPUT_VARIABLE GIT_BRANCH
+  OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+
+configure_file( ${CMAKE_CURRENT_LIST_DIR}/../src/pylimer_tools_cpp/version_config.h.in ${CMAKE_BINARY_DIR}/generated/version_config.h )
+include_directories( ${CMAKE_BINARY_DIR}/generated/ )
