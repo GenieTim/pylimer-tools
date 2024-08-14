425190c8f1da34a7d1015a08f99f743fb0fb92eb
Tim Bernhard <tim@bernhard-webstudio.ch>
2024-08-14 21:53:27 +0200

    Stop randomly sampling chain ends if cross-links sampling is disabled

--- src/pylimer_tools_cpp/sim/MEHPForceBalance.h
+++ src/pylimer_tools_cpp/sim/MEHPForceBalance.h
@@ -362,7 +362,8 @@ namespace sim {
                                       minimumNrOfSliplinks,
                                       sameStrandCutoff,
                                       seed,
-                                      crossLinkerType);
+                                      crossLinkerType,
+                                      true);
 
         RUNTIME_EXP_IFN(
           entanglements.pairsOfAtoms.size() >= minimumNrOfSliplinks,
