cec0135d6689931e989b230ded2bf85b5539821c
Tim Bernhard <tim@bernhard-webstudio.ch>
2024-12-02 14:47:32 +0100

    Get test coverage of python tests working again

--- tests/__init__.py
+++ tests/__init__.py
@@ -3,4 +3,4 @@ import sys
 
 sys.path.append(os.path.dirname(__file__))
 # sys.path.insert(0, os.path.dirname(__file__ ) + "/../src/pylimer_tools/calc")
-# sys.path.insert(0, os.path.dirname(__file__ ) + "/../src/pylimer_tools/")
+sys.path.insert(0, os.path.dirname(__file__) + "/../src/pylimer_tools/")
