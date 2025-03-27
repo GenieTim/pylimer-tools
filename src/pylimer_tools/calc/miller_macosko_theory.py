97c8fb394b95addff3a2893fb655bd561d62a159
Tim Bernhard <tim@bernhard-webstudio.ch>
2025-03-27 13:58:02 +0100

    Expose more details of the units

--- src/pylimer_tools/calc/miller_macosko_theory.py
+++ src/pylimer_tools/calc/miller_macosko_theory.py
@@ -42,7 +42,8 @@ def predict_shear_modulus(**kwargs):
     ToDo:
       - Support more than one crosslinker type (as is supported by original formula)
     """
-    g_mmt_phantom, g_mmt_entanglement, _, _ = compute_modulus_decomposition(**kwargs)
+    g_mmt_phantom, g_mmt_entanglement, _, _ = compute_modulus_decomposition(
+        **kwargs)
     return g_mmt_phantom + g_mmt_entanglement
 
 
@@ -383,10 +384,12 @@ def compute_weight_fraction_of_soluble_material_from_weight_fractions(
       - g: the functionality of the ordinary chains
     """
     alpha, _ = compute_miller_macosko_probabilities(r, p, f)
-    return w_f * (alpha**f) + w_g * ((r * p * (alpha ** (f - 1)) + 1 - r * p) ** g)
+    return w_f * (alpha**f) + w_g * \
+        ((r * p * (alpha ** (f - 1)) + 1 - r * p) ** g)
 
 
-def compute_miller_macosko_probabilities(r: float, p: float, f: int, b2: float = 1.0):
+def compute_miller_macosko_probabilities(
+        r: float, p: float, f: int, b2: float = 1.0):
     """
     Compute Macosko and Miller's probabilities :math:`P(F_A)` and :math:`P(F_B)`
     i.e., the probability that a randomly chosen A (cross-link) or B (strand-end),
@@ -433,7 +436,8 @@ def compute_miller_macosko_probabilities(r: float, p: float, f: int, b2: float =
     if f == 3:
         alpha = (1 - r * p * p * b2) / (r * p * p * b2)
     elif f == 4:
-        alpha = ((1.0 / (r * p * p * b2)) - 3.0 / 4.0) ** (1.0 / 2.0) - (1.0 / 2.0)
+        alpha = ((1.0 / (r * p * p * b2)) - 3.0 /
+                 4.0) ** (1.0 / 2.0) - (1.0 / 2.0)
     else:
         if not (f > 4):
             raise NotImplementedError(
@@ -441,7 +445,8 @@ def compute_miller_macosko_probabilities(r: float, p: float, f: int, b2: float =
             )
 
         def fun_to_root_for_alpha(alpha):
-            return r * b2 * p**2 * alpha ** (f - 1) - alpha - r * b2 * (p**2) + 1
+            return r * b2 * p**2 * \
+                alpha ** (f - 1) - alpha - r * b2 * (p**2) + 1
 
         def fun_to_root_for_alpha_prime(alpha):
             return -1 + alpha ** (f - 2) * (-1 + f) * (p**2) * r * b2
@@ -727,10 +732,12 @@ def compute_probability_that_crosslink_is_effective(
     f = functionality_of_monomer
     m = expected_degree_of_effect
     alpha = p_f_a_out
-    return scipy.special.binom(f, m) * (alpha ** (f - m)) * ((1.0 - alpha) ** m)
+    return scipy.special.binom(
+        f, m) * (alpha ** (f - m)) * ((1.0 - alpha) ** m)
 
 
-def compute_probability_that_bifunctional_monomer_is_effective(p_f_b_out: float):
+def compute_probability_that_bifunctional_monomer_is_effective(
+        p_f_b_out: float):
     """
     Consider a copolymerization of A_f with B_2.
     This function computes the probability that a random B_2 unit will be effective.
@@ -764,7 +771,8 @@ def compute_probability_that_crosslink_with_degree_is_dangling(
     alpha = p_f_a_out
     # NOTE: verify that the last exponent is f - m, rather than f - 1 as in
     # the paper
-    return scipy.special.binom(f, i) * (alpha ** (i)) * ((1.0 - alpha) ** (f - i))
+    return scipy.special.binom(f, i) * (alpha ** (i)) * \
+        ((1.0 - alpha) ** (f - i))
 
 
 def compute_probability_that_crosslink_is_dangling(
@@ -788,7 +796,8 @@ def compute_probability_that_crosslink_is_dangling(
     return scipy.special.binom(f, 1) * (alpha ** (f - 1)) * (1.0 - alpha)
 
 
-def compute_probability_that_bifunctional_monomer_is_dangling(p_f_b_out: float):
+def compute_probability_that_bifunctional_monomer_is_dangling(
+        p_f_b_out: float):
     """
     Consider a copolymerization of A_f with B_2.
     This function computes the probability that a random B_2 unit will be dangling.
@@ -891,11 +900,13 @@ def predict_p_from_w_sol(
 def _validate_r_and_p(r: float, p: float, f: int):
     if p < 0:
         raise ValueError(
-            "The cross-linker conversion `p` must be positive, got {}".format(p)
+            "The cross-linker conversion `p` must be positive, got {}".format(
+                p)
         )
     if r < 0:
         raise ValueError(
-            "The stoichiometric imbalance `r` must be positive, got {}".format(r)
+            "The stoichiometric imbalance `r` must be positive, got {}".format(
+                r)
         )
     if f < 2:
         raise ValueError(
@@ -945,7 +956,9 @@ _validators_assembler = [
     ),
     _ParamValidatorAssembler(
         "crosslinker_type",
-        lambda p: max(p["functionality_per_type"], key=p["functionality_per_type"].get),
+        lambda p: max(
+            p["functionality_per_type"],
+            key=p["functionality_per_type"].get),
         lambda x: isinstance(x, int) and x >= 0,
         ["functionality_per_type"],
     ),
@@ -1039,7 +1052,8 @@ def _compute_validate_parameters(
         return all(_param_is_ready(dep) for dep in param.dependencies)
 
     def _validate(param_name: str):
-        if not _validator_per_name[param_name].param_validator(given_parameters[p]):
+        if not _validator_per_name[param_name].param_validator(
+                given_parameters[p]):
             raise ValueError(
                 "Invalid value for parameter '{}' (got {}).".format(
                     param_name, given_parameters[param_name]
@@ -1052,7 +1066,8 @@ def _compute_validate_parameters(
         _validate(p)
 
     # first, determine all parameters to compute
-    to_compute = set([d for d in required_parameters if not _param_is_ready(d)])
+    to_compute = set(
+        [d for d in required_parameters if not _param_is_ready(d)])
     # add dependencies
     found_last_iteration = True
     while found_last_iteration:
