1cefd0dfba02774c6bc67703e1af43d09d71bec3
Tim Bernhard <tim@bernhard-webstudio.ch>
2025-05-12 11:38:30 +0200

    Commit missing file

--- src/pylimer_tools_cpp/topo/TopologyCalc.h
+++ src/pylimer_tools_cpp/topo/TopologyCalc.h
@@ -6,11 +6,12 @@
 namespace pylimer_tools {
 namespace topo {
 
-  static inline Eigen::Vector3d sampleIntersectionPoint(const Eigen::Vector3d origin1,
-                                          const double radius1,
-                                          const Eigen::Vector3d origin2,
-                                          const double radius2,
-                                          double theta = 0.0)
+  static inline Eigen::Vector3d sampleIntersectionPoint(
+    const Eigen::Vector3d origin1,
+    const double radius1,
+    const Eigen::Vector3d origin2,
+    const double radius2,
+    double theta = 0.0)
   {
     // after https://gamedev.stackexchange.com/a/75775
     double d = (origin2 - origin1).norm();
@@ -103,13 +104,14 @@ namespace topo {
     return false;
   };
 
-  static inline bool segmentIntersectsTriangle(const Eigen::Vector3d rayOrigin,
-                                 const Eigen::Vector3d rayTarget,
-                                 const Eigen::Vector3d vertex0,
-                                 const Eigen::Vector3d vertex1,
-                                 const Eigen::Vector3d vertex2,
-                                 Eigen::Vector3d& outIntersectionPoint,
-                                 const double EPSILON = 1e-6)
+  static inline bool segmentIntersectsTriangle(
+    const Eigen::Vector3d rayOrigin,
+    const Eigen::Vector3d rayTarget,
+    const Eigen::Vector3d vertex0,
+    const Eigen::Vector3d vertex1,
+    const Eigen::Vector3d vertex2,
+    Eigen::Vector3d& outIntersectionPoint,
+    const double EPSILON = 1e-6)
   {
     return segmentIntersectsTriangle(
       rayOrigin,
