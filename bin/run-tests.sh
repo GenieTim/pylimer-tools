1b6f1cf29b995918f345b5475382ed2f2a3ca31f
Tim Bernhard <tim@bernhard-webstudio.ch>
2025-05-15 22:08:17 +0200

    Next attempts to fix CI

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -21,16 +21,19 @@ if command -v ninja; then
 fi
 
 echo "======== Starting tests ========"
-# MallocNanoZone=0 ASAN_OPTIONS=detect_leaks=1:detect_container_overflow=0:strict_string_checks=1:detect_stack_use_after_return=1:check_initialization_order=1:strict_init_order=1 LSAN_OPTIONS=suppressions=$ROOT_DIR/tests/lsan.supp ./pylimer_tests "[MEHPForceBalance]" || exit 5 # -s --durations yes
-MallocNanoZone=0 ASAN_OPTIONS=detect_leaks=0:detect_container_overflow=0:strict_string_checks=1:detect_stack_use_after_return=1:check_initialization_order=1:strict_init_order=1 LSAN_OPTIONS=suppressions=$ROOT_DIR/tests/lsan.supp ./pylimer_tests --skip-benchmarks --benchmark-samples 10 --durations yes || exit 6 #  -s --durations yes
+MallocNanoZone=0 ASAN_OPTIONS=detect_leaks=0:detect_container_overflow=0:strict_string_checks=1:detect_stack_use_after_return=1:check_initialization_order=1:strict_init_order=1 LSAN_OPTIONS=suppressions=$ROOT_DIR/tests/lsan.supp \
+  time "$GENERATOR_BIN" pylimer_tests-gcov
 
-MallocNanoZone=0 ASAN_OPTIONS=detect_leaks=0:detect_container_overflow=0:strict_string_checks=1:detect_stack_use_after_return=1:check_initialization_order=1:strict_init_order=1 LSAN_OPTIONS=suppressions=$ROOT_DIR/tests/lsan.supp time "$GENERATOR_BIN" pylimer_tests-gcov
+MallocNanoZone=0 ASAN_OPTIONS=detect_leaks=0:detect_container_overflow=0:strict_string_checks=1:detect_stack_use_after_return=1:check_initialization_order=1:strict_init_order=1 LSAN_OPTIONS=suppressions=$ROOT_DIR/tests/lsan.supp \
+  ./pylimer_tests --skip-benchmarks --benchmark-samples 10 --durations yes --order rand \
+    --reporter JUnit::out="$ROOT_DIR/test-reports/pylimer-tests-result-junit.xml" \
+    --reporter console::out=-::colour-mode=ansi || exit 6
+
 find . -name "*Universe.cpp.gcov" -exec cat {} \;
 
 cd "$ROOT_DIR" || exit 8
 
 # copy outside such that pip installation does not remove it
-# cp tests/build/lcov/data/capture/pylimer_tools.info pylimer_tools.info
 
 if command -v npx; then
   npx -y lcov-badge2 -l "C++ Code Coverage" -o ".github/cpp-coverage.svg" tests/build/lcov/data/capture/pylimer_tools.info || echo "Failed to generate coverage badge"
