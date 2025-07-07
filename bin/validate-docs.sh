8cd2d01aa2685b0c543006a82c39c61a1c69df42
Tim Bernhard <tim@bernhard-webstudio.ch>
2025-07-07 07:57:06 +0200

    Implement a check to validate the rst documents

---
+++ bin/validate-docs.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+cd "$(dirname "$0")/.." || exit 10
+# ROOT_DIR=$(pwd)
+
+# iterate over all *.rst files in the docs directory
+for file in docs/**/*.rst; do
+  # check if the file exists
+  if [[ -f "$file" ]]; then
+    # validate the file using rstcheck
+    rstcheck "$file" --ignore-directives automodule,autosummary,autoclass,autofunction,autocode || {
+      echo "Validation failed for $file. Please check the output above."
+      echo "--------------------------"
+      echo ""
+    }
+  else
+    echo "File $file does not exist."
+    exit 2
+  fi
+done
