3b6224d48eb07cccbae4eaa8d00a22e5f9ea5c11
GenieTim <info@bernhard-webstudio.ch>
2025-07-29 14:31:16 +0000

    Deploying to gh-pages from @ GenieTim/pylimer-tools@3b368f9d3ffeefc3245ff1560f00f92a1d8da510 🚀

--- _downloads/cc2d57636e7adfbcc8bef1a31e802048/plot_trajectory_analysis.py
+++ _downloads/cc2d57636e7adfbcc8bef1a31e802048/plot_trajectory_analysis.py
@@ -28,7 +28,7 @@ sequence.initialize_from_dump_file(
 # Compute the mean square displacement for all atoms
 msd = sequence.compute_msd_for_atoms(
     atom_ids=[a.get_id() for a in sequence[0].get_atoms()],
-    nr_of_origins=10,  # Number of origins to use for MSD calculation
+    nr_of_origins=50,  # Number of origins to use for MSD calculation
 )
 
 # %%
@@ -42,8 +42,14 @@ rgs = []
 timesteps = []
 for universe in sequence:
     rgs.append(
-        np.mean([m.compute_radius_of_gyration()
-                for m in universe.get_molecules(2)])
+        np.mean(
+            [
+                m.compute_radius_of_gyration()
+                for m in universe.get_molecules(
+                    -1
+                )  # -1 because we don't have any crosslinks to filter out
+            ]
+        )
     )
     timesteps.append(universe.get_timestep())
 
@@ -55,16 +61,40 @@ axs[0].plot(
     list(
         msd.keys()), list(
             msd.values()), label="Mean Square Displacement")
+axs[0].axhline(
+    np.mean(
+        list(
+            msd.values())),
+    color="black",
+    linestyle="--",
+    label="Mean")
+# add cumulative mean
+axs[0].plot(
+    list(msd.keys()),
+    [np.mean(list(msd.values())[: i + 1]) for i in range(len(msd))],
+    label="Cumulative Mean",
+    color="red",
+)
 axs[0].set_xlabel("$\\tau$")
 axs[0].set_ylabel("MSD")
 axs[0].set_title("Mean Square Displacement")
+axs[0].set(xscale="log", yscale="log")
 axs[0].legend()
 
 # Panel 2: Radius of Gyration
 axs[1].plot(timesteps, rgs, label="Radius of Gyration", color="orange")
+axs[1].axhline(np.mean(rgs), color="black", linestyle="--", label="Mean")
+# add cumulative mean
+axs[1].plot(
+    timesteps,
+    [np.mean(rgs[: i + 1]) for i in range(len(rgs))],
+    label="Cumulative Mean",
+    color="red",
+)
 axs[1].set_xlabel("Timestep")
 axs[1].set_ylabel("Radius of Gyration")
 axs[1].set_title("Radius of Gyration Over Time")
+axs[1].set(xscale="log", yscale="log")
 axs[1].legend()
 
 plt.tight_layout()
